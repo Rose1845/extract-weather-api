f84259a26630f66912b87b60414db6f979f1cdaa
Rose1845 <odhiamborose466@gmail.com>
2026-05-17 14:02:47 +0300

    feat: Add logging configuration and integrate logging into Kafka consumer for better error tracking

--- consumer/consumer_config.py
+++ consumer/consumer_config.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 import json
 from config.app_config import settings
 from config.cassandra_config import connect_cassandra, prepare_insert, insert_weather
+from config.log import log
 
 
 def create_consumer():
@@ -18,4 +19,35 @@ def create_consumer():
 
 def exceute_consumer():
     session = connect_cassandra()
-    consumner = create_consumer()
+    insert_record_data = prepare_insert(session)
+    consumer = create_consumer()
+
+    try:
+        for message in consumer:
+            record = message.value
+
+            log.info(f"record: {record}")
+            try:
+                insert_weather(session, insert_record_data, record)
+                consumer.commit()
+                log.info(
+                    record["city"],
+                    record["timestamp"],
+                    record["temperature"],
+                    record["description"],
+                )
+
+            except Exception as e:
+                log.error(
+                    "Failed to write record for %s: %s",
+                    record.get("city", "?"),
+                    e,
+                )
+    except Exception as e:
+        log.error(
+            "An error occured: %s: %s"
+        )
+
+    finally:
+        consumer.close()
+        log.info("Consumer closed.")
